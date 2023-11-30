440ee78de490b60354aff0663e4f027abdf3d860
Chowdhury Md Rajib Sarwar <rajib@limosys.com>
2023-11-30 12:20:05 -0500

    created a new project to explore core data from basic to advanced

---
+++ HitList/HitList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by Chowdhury Md Rajib Sarwar on 11/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
