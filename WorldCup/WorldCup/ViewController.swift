7532f5a42e0cb1aee9850737eb50369a05190c3b
Rajib-Sarwar <md.rajib.sarwar@gmail.com>
2024-01-08 23:08:25 -0500

    integrated fetchviewcontrollerresult deletegate and later updated the same behaviour using diffable data source

--- WorldCup/WorldCup/ViewController.swift
+++ WorldCup/WorldCup/ViewController.swift
@@ -35,7 +35,24 @@ class ViewController: UIViewController {
   // MARK: - Properties
   private let teamCellIdentifier = "teamCellReuseIdentifier"
   lazy var  coreDataStack = CoreDataStack(modelName: "WorldCup")
-
+  lazy var fetchedResultsController: NSFetchedResultsController<Team> = {
+    let fetchRequest: NSFetchRequest<Team> = Team.fetchRequest()
+    let zoneSort = NSSortDescriptor(key: #keyPath(Team.qualifyingZone), ascending: true)
+    let scoreSort = NSSortDescriptor(key: #keyPath(Team.wins), ascending: false)
+    let nameSort = NSSortDescriptor(key: #keyPath(Team.teamName), ascending: true)
+    fetchRequest.sortDescriptors = [zoneSort, scoreSort, nameSort]
+    
+    let fetchedResultsController = NSFetchedResultsController(
+      fetchRequest: fetchRequest,
+      managedObjectContext: coreDataStack.managedContext,
+      sectionNameKeyPath: #keyPath(Team.qualifyingZone),
+      cacheName: "WorldCup")
+    fetchedResultsController.delegate = self
+    return fetchedResultsController
+  }()
+  
+  var dataSource: UITableViewDiffableDataSource<String, NSManagedObjectID>?
+  
   // MARK: - IBOutlets
   @IBOutlet weak var tableView: UITableView!
   @IBOutlet weak var addButton: UIBarButtonItem!
@@ -45,6 +62,64 @@ class ViewController: UIViewController {
     super.viewDidLoad()
 
     importJSONSeedDataIfNeeded()
+    
+    dataSource = setupDataSource()
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    UIView.performWithoutAnimation {
+      do {
+        try fetchedResultsController.performFetch()
+      }  catch let error as NSError {
+        print("Could not fetch: \(error), \(error.userInfo)")
+      }
+    }
+  }
+  
+  override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+    if motion == .motionShake {
+      addButton.isEnabled = true
+    }
+  }
+}
+
+// MARK: IBAction
+extension ViewController {
+  @IBAction func addTeam(_ sender: Any) {
+    let alertController = UIAlertController(
+      title: "Secret Team",
+      message: "Add a new team",
+      preferredStyle: .alert)
+    
+    alertController.addTextField { textField in
+      textField.placeholder = "Team Name"
+    }
+    
+    alertController.addTextField { textField in
+      textField.placeholder = "Qualifying Zone"
+    }
+    
+    let saveAction = UIAlertAction(
+      title: "Save",
+      style: .default) { [unowned self] _ in
+        guard
+          let nameTextField = alertController.textFields?.first,
+          let zoneTextField = alertController.textFields?.last else {
+          return
+        }
+        
+        let team = Team(context: self.coreDataStack.managedContext)
+        team.teamName = nameTextField.text
+        team.qualifyingZone = zoneTextField.text
+        team.imageName = "scoreboard-flag"
+        self.coreDataStack.saveContext()
+      }
+    
+    alertController.addAction(saveAction)
+    alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+    
+    present(alertController, animated: true, completion: nil)
   }
 }
 
@@ -55,32 +130,93 @@ extension ViewController {
       return
     }
 
-    cell.flagImageView.backgroundColor = .blue
-    cell.teamLabel.text = "Team Name"
-    cell.scoreLabel.text = "Wins: 0"
+    let team = fetchedResultsController.object(at: indexPath)
+    cell.teamLabel.text = team.teamName
+    cell.scoreLabel.text = "Wins: \(team.wins)"
+    
+    if let imageName = team.imageName {
+      cell.flagImageView.image = UIImage(named: imageName)
+    } else {
+      cell.flagImageView.image = nil
+    }
+  }
+  
+  func configure(cell: UITableViewCell, for team: Team) {
+    guard let cell = cell as? TeamCell else {
+      return
+    }
+
+    cell.teamLabel.text = team.teamName
+    cell.scoreLabel.text = "Wins: \(team.wins)"
+    
+    if let imageName = team.imageName {
+      cell.flagImageView.image = UIImage(named: imageName)
+    } else {
+      cell.flagImageView.image = nil
+    }
+  }
+  
+  func setupDataSource() -> UITableViewDiffableDataSource<String, NSManagedObjectID> {
+    UITableViewDiffableDataSource(tableView: tableView) { [unowned self] (tableView, indexPath, managedObjectId) -> UITableViewCell? in
+      let cell = tableView.dequeueReusableCell(withIdentifier: self.teamCellIdentifier, for: indexPath)
+      if let team = try? coreDataStack.managedContext.existingObject(with: managedObjectId) as? Team {
+        self.configure(cell: cell, for: team)
+      }
+      return cell
+    }
   }
 }
 
-// MARK: - UITableViewDataSource
-extension ViewController: UITableViewDataSource {
-  func numberOfSections(in tableView: UITableView) -> Int {
-    1
-  }
-
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    20
-  }
-
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: teamCellIdentifier, for: indexPath)
-    configure(cell: cell, for: indexPath)
-    return cell
-  }
-}
+//// MARK: - UITableViewDataSource
+//extension ViewController: UITableViewDataSource {
+//  func numberOfSections(in tableView: UITableView) -> Int {
+//    fetchedResultsController.sections?.count ?? 0
+//  }
+//  
+//  func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//    let sectionInfo = fetchedResultsController.sections?[section]
+//    return sectionInfo?.name
+//  }
+//
+//  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//    guard let sectionInfo = fetchedResultsController.sections?[section] else {
+//      return 0
+//    }
+//    
+//    return sectionInfo.numberOfObjects
+//  }
+//
+//  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//    let cell = tableView.dequeueReusableCell(withIdentifier: teamCellIdentifier, for: indexPath)
+//    configure(cell: cell, for: indexPath)
+//    return cell
+//  }
+//}
 
 // MARK: - UITableViewDelegate
 extension ViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    let sectionInfo = fetchedResultsController.sections?[section]
+    
+    let titleLabel = UILabel()
+    titleLabel.backgroundColor = .systemGroupedBackground
+    titleLabel.text = sectionInfo?.name
+    
+    return titleLabel
+  }
+  
+  func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    20
+  }
+  
   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let team = fetchedResultsController.object(at: indexPath)
+    team.wins += 1
+    if var snapshot = dataSource?.snapshot() {
+      snapshot.reloadItems([team.objectID])
+      dataSource?.apply(snapshot, animatingDifferences: true)
+    }
+    coreDataStack.saveContext()
   }
 }
 
@@ -127,4 +263,50 @@ extension ViewController {
     }
   }
   // swiftlint:enable force_unwrapping force_cast force_try
+}
+
+extension ViewController: NSFetchedResultsControllerDelegate {
+  
+//  func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+//    tableView.beginUpdates()
+//  }
+//  
+//  func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+//    let indexSet = IndexSet(integer: sectionIndex)
+//    
+//    switch type {
+//    case .insert:
+//      tableView.insertSections(indexSet, with: .automatic)
+//    case .delete:
+//      tableView.deleteSections(indexSet, with: .automatic)
+//    default: break
+//    }
+//  }
+//  
+//  func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+//    switch type {
+//    case .insert:
+//      tableView.insertRows(at: [newIndexPath!], with: .automatic)
+//    case .delete:
+//      tableView.deleteRows(at: [indexPath!], with: .automatic)
+//    case .update:
+//      let cell = tableView.cellForRow(at: indexPath!) as! TeamCell
+//      configure(cell: cell, for: indexPath!)
+//    case .move:
+//      tableView.deleteRows(at: [indexPath!], with: .automatic)
+//      tableView.insertRows(at: [newIndexPath!], with: .automatic)
+//    @unknown default:
+//      print("Unexpected NSFetchedResultsChangeType")
+//    }
+//    
+//  }
+//  
+//  func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+//    tableView.endUpdates()
+//  }
+  
+  func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChangeContentWith snapshot: NSDiffableDataSourceSnapshotReference) {
+    let snapshot = snapshot as NSDiffableDataSourceSnapshot<String, NSManagedObjectID>
+    dataSource?.apply(snapshot)
+  }
 }
